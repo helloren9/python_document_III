682748cef717b5723bb2b52293d5d134e0d84b31
Loren Anunciacion <loren.anunciacion@gmail.com>
2024-02-27 16:20:05 -0800

    Add files via upload

---
+++ module.py
@@ -0,0 +1,8 @@
+def printName(name):
+    return f"Hello Mr/Ms {name}...we've been waiting for you!"
+    
+def square_feet(length, width):
+    return length * width
+
+def circle(radius):
+    return 2 * 3.14 * radius
